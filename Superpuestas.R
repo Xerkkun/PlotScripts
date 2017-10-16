4092a95ef3f71f0e737cece7c9fefb6db2960725
Fer Moreno López <xerk.kun@gmail.com>
2017-10-15 20:49:27 -0700

    Add files via upload

---
+++ Superpuestas.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(reshape2)
+
+# original data in a 'wide' format
+x  <- seq(-2, 2, 0.05)
+y1 <- pnorm(x)
+y2 <- pnorm(x, 1, 1)
+df <- data.frame(x, y1, y2)
+
+# melt the data to a long format
+df2 <- melt(data = df, id.vars = "x")
+
+# plot, using the aesthetics argument 'colour'
+ggplot(data = df2, aes(x = x, y = value, colour = variable)) + geom_line()
